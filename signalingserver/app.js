5db44b5ed6b31f8e5893f5e0f9ae977ca0ed10da
log G Cheng <logg.com@gmail.com>
2020-09-30 20:31:57 +0800

    my change

--- signalingserver/app.js
+++ signalingserver/app.js
@@ -4,57 +4,59 @@ var sockets;
 const http = require("http");
 const WebSocket = require("ws"); // See https://github.com/websockets/ws
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 startServer();
 
-function startServer()
-{
+function startServer() {
   var server, wss;
-
-  server = http.createServer(dummyStatusMessage);
-  wss = new WebSocket.Server({server: server});
-  sockets = {};
-  wss.on("connection", initializeConnection);
-  server.listen(port);
+  try {
+    server = http.createServer(dummyStatusMessage);
+    wss = new WebSocket.Server({ server: server });
+    sockets = {};
+    wss.on("connection", initializeConnection);
+    console.log(port);
+    server.listen(port);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function dummyStatusMessage(request, response)
-{
-  response.writeHead(200, {"Content-Type": "text/plain"});
+function dummyStatusMessage(request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
   response.end("Signaling server is running.");
 }
 
-function initializeConnection(socket)
-{
+function initializeConnection(socket) {
   var clientId;
 
-  clientId = Math.floor(Math.random()*1000000000);
-  console.log("\nNew client connected.  Assigning ID %d.", clientId)
+  clientId = Math.floor(Math.random() * 1000000000);
+  console.log("\nNew client connected.  Assigning ID %d.", clientId);
   socket.onmessage = forwardMessage;
   sockets[clientId] = socket;
-  socket.send(JSON.stringify({id: clientId}));
+  socket.send(JSON.stringify({ id: clientId }));
 }
 
-function forwardMessage(event)
-{
+function forwardMessage(event) {
   var message;
 
   message = JSON.parse(event.data);
 
-  if (!message.to)
-  {
+  if (!message.to) {
     console.log("\nDiscarding message without recipient.");
     return;
   }
 
-  if (!sockets[message.to])
-  {
+  if (!sockets[message.to]) {
     console.log("\nDiscarding message to unknown recipient %s.", message.to);
     return;
   }
 
-  console.log("\nForwarding message from %s to %s: %s",
-    message.from, message.to, event.data);
+  console.log(
+    "\nForwarding message from %s to %s: %s",
+    message.from,
+    message.to,
+    event.data
+  );
   sockets[message.to].send(event.data);
 }
--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,24 +1,27 @@
 "use strict";
 
-const https   = require("https");
-const pem     = require("pem");
+const https = require("https");
+const pem = require("pem");
 const express = require("express");
-const fs      = require("fs");
+const fs = require("fs");
 
 const app = express();
 app.use(express.static("../frontend"));
-pem.createCertificate({
-  days:               1,
-  commonName:         "127.0.0.1",
-  serviceCertificate: fs.readFileSync("ca.crt"),
-  serviceKey:         fs.readFileSync("ca.key")}, callback);
-    
-function callback(err, keys)
-{
+pem.createCertificate(
+  {
+    days: 1,
+    commonName: "127.0.0.1",
+    serviceCertificate: fs.readFileSync("cert.pem"),
+    serviceKey: fs.readFileSync("key.pem"),
+  },
+  callback
+);
+
+function callback(err, keys) {
   if (err) throw err;
 
   https
-    .createServer({key: keys.clientKey, cert: keys.certificate}, app)
+    .createServer({ key: keys.clientKey, cert: keys.certificate }, app)
     .listen(4300);
 
   console.log("Listening on https://127.0.0.1:4300");
